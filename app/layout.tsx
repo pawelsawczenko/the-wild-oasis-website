d745302bc3b8f4984b5e94d81596e7addc09d2b2
pawelsawczenko <pawelsawczenko@gmail.com>
2024-09-24 20:40:24 +0300

    add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,12 @@ import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "The Wild Oasis Website",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Weclome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and darks forests",
 };
 
 export default function RootLayout({
