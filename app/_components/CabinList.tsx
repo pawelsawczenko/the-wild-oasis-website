63f32a2f073489f002948e632b89e59945e6476b
pawelsawczenko <pawelsawczenko@gmail.com>
2024-09-29 21:12:15 +0300

    Add filter to cabins

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -2,14 +2,47 @@ import CabinCard from "../_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 import { Cabin } from "../_lib/definitions";
 
-export default async function CabinList() {
+interface CabinListProps {
+  filter: string;
+}
+export default async function CabinList({ filter }: CabinListProps) {
   const cabins: Cabin[] = await getCabins();
 
   if (!cabins.length) return null;
 
+  const displayCabins: Cabin[] =
+    filter === "all"
+      ? cabins
+      : filter === "small"
+      ? cabins.filter((cabin) => cabin.maxCapacity && cabin.maxCapacity <= 3)
+      : filter === "medium"
+      ? cabins.filter(
+          (cabin) =>
+            cabin.maxCapacity &&
+            cabin.maxCapacity >= 4 &&
+            cabin.maxCapacity <= 7
+        )
+      : filter === "large"
+      ? cabins.filter((cabin) => cabin.maxCapacity && cabin.maxCapacity >= 8)
+      : [];
+  // if (filter === "all") displayCabins = cabins;
+  // if (filter === "small")
+  //   displayCabins = cabins.filter(
+  //     (cabin) => cabin.maxCapacity && cabin.maxCapacity <= 3
+  //   );
+  // if (filter === "medium")
+  //   displayCabins = cabins.filter(
+  //     (cabin) =>
+  //       cabin.maxCapacity && cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+  //   );
+  // if (filter === "large")
+  //   displayCabins = cabins.filter(
+  //     (cabin) => cabin.maxCapacity && cabin.maxCapacity >= 12
+  //   );
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,14 +1,19 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
-
-export const revalidate = 3600;
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+interface CabinsProps {
+  searchParams: { capacity?: string };
+}
+
+export default function Page({ searchParams }: CabinsProps) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +27,13 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
