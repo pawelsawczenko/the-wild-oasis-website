8e5fb20ba35b905ece94bc8571516e4d8294d551
pawelsawczenko <pawelsawczenko@gmail.com>
2025-02-09 20:15:08 +0300

    fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
-    default: "Weclome / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
   },
   description:
     "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and darks forests",
